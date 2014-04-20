04c3ac0a770ebd013848e72d05ac72b9e099f502
Charlie Somerville <charlie@charliesomerville.com>
2014-04-20 17:55:21 +1000

    we have an AST but not a parser

--- main.rs
+++ main.rs
@@ -1,8 +1,75 @@
 use std::io;
 
+mod ast {
+    pub trait Node {
+        fn eval(&self) -> f64;
+    }
+
+    pub struct Num {
+        pub num: f64
+    }
+
+    impl Node for Num {
+        fn eval(&self) -> f64 {
+            self.num
+        }
+    }
+
+    pub struct Add {
+        pub left: ~Node,
+        pub right: ~Node,
+    }
+
+    impl Node for Add {
+        fn eval(&self) -> f64 {
+            self.left.eval() + self.right.eval()
+        }
+    }
+
+    pub struct Sub {
+        pub left: ~Node,
+        pub right: ~Node,
+    }
+
+    impl Node for Sub {
+        fn eval(&self) -> f64 {
+            self.left.eval() - self.right.eval()
+        }
+    }
+
+    pub struct Mul {
+        pub left: ~Node,
+        pub right: ~Node,
+    }
+
+    impl Node for Mul {
+        fn eval(&self) -> f64 {
+            self.left.eval() * self.right.eval()
+        }
+    }
+
+    pub struct Div {
+        pub left: ~Node,
+        pub right: ~Node,
+    }
+
+    impl Node for Div {
+        fn eval(&self) -> f64 {
+            self.left.eval() / self.right.eval()
+        }
+    }
+}
+
+fn parse(input: &str) -> ~ast::Node {
+    // just return a static ast for now
+    ~ast::Add {
+        left: ~ast::Num { num: 1.0 } as ~ast::Node,
+        right: ~ast::Num { num: 2.0 } as ~ast::Node,
+    } as ~ast::Node
+}
+
 fn evaluate(input: &str) -> ~str {
-    // do nothing with the input for now
-    input.to_owned()
+    parse(input).eval().to_str()
 }
 
 pub fn main() {
